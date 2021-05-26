0e22c7b9a4d7cc24298044825cbb5de535c93e79
manvith <iib2019014@iiita.ac.in>
2021-05-26 12:54:52 +0530

    1) added media_url, media_root in settings.py 2) extended urlpatterns with media_url in urls.py of root fldr, 3) image is not coming in user page

--- accounts/models.py
+++ accounts/models.py
@@ -21,6 +21,7 @@ class Customer(models.Model) :
     name = models.CharField(max_length=25, null=True)
     phone = models.CharField(max_length=10, null=True)
     email = models.CharField(max_length=25, null=True)
+    profile_pic = models.ImageField(null=True, blank=True)
 
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
